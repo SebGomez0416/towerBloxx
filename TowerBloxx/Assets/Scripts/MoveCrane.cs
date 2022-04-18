0115e723b2e6209f4ae1023974f42917b08646de
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-04-18 05:09:35 -0300

    Add Move Crane

---
+++ TowerBloxx/Assets/Scripts/MoveCrane.cs
@@ -0,0 +1,26 @@
+
+using System;
+using UnityEngine;
+
+public class MoveCrane : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float initTime;
+    [SerializeField] private float time;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("Move",initTime,time);
+    }
+
+    private void Move()
+    {
+        rb.AddForce(Vector3.forward*speed,ForceMode.Impulse);
+        Debug.Log("entro");
+    }
+}
